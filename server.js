664911100ac3a62728171a8a98f89efc868d7be1
Alex Phimister <wphimmy@gmail.com>
2020-12-10 15:41:38 +0000

    deleted api key and hid it using dotenv node package. In order to use this program you will have to generate your own .env file in the repo and enter your own api-key. also added stroud weather forecast

--- server.js
+++ server.js
@@ -2,8 +2,22 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const axios = require("axios");
+const dotenv = require("dotenv");
 
-const apiKey = "3a71f52109ea13652c84221c839f41f0";
+dotenv.config({ path: "./.env" });
+
+const apiKey = process.env.WEATHER_KEY,
+  lat = 51.7457,
+  lon = 2.2178,
+  weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
 const viewsPath = path.join(__dirname, "/views");
 
@@ -44,8 +58,33 @@ app.post("/formResult", async (req, res) => {
   }
 });
 
+app.get("/Stroud", async (req, res) => {
+  try {
+    const myApi = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=3a71f52109ea13652c84221c839f41f0&units=metric`
+    );
+    const daily = myApi.data.daily;
+
+    const days = daily.map((a) => {
+      console.log(a);
+      let date = new Date(a.dt * 1000);
+      let day = weekdays[date.getUTCDay()];
+      return {
+        day: day,
+        temp: a.temp.day,
+        description: a.weather[0].description,
+        icon: `http://openweathermap.org/img/wn/${a.weather[0].icon}@2x.png`,
+      };
+    });
+    res.render("stroudWeather", { weatherObject: days });
+  } catch (err) {
+    console.log(err);
+    res.send("<h1>Unknown Error, please report to Alex</h1>");
+  }
+});
+
 app.get("*", (req, res) => {
-  res.send("<h1>404, request not found</h1>");
+  res.send("<h1>Page not found</h1>");
 });
 
 app.listen(5000, () => {

f9a070d9916b324632fbe7a6392e1b1809c29ed8
Alex Phimister <wphimmy@gmail.com>
2020-12-11 11:07:25 +0000

    Changed routing so evrything is on one url. Added dropdown to assist with navigation. Added to partials to DRY out the code

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const hbs = require("hbs");
 const path = require("path");
 const axios = require("axios");
 const dotenv = require("dotenv");
@@ -17,14 +18,16 @@ const apiKey = process.env.WEATHER_KEY,
     "Saturday",
   ];
 
-console.log(apiKey);
 const viewsPath = path.join(__dirname, "/views");
 
 app.set("view engine", "hbs");
 
 app.set("views", viewsPath);
 
-const publicDirectory = path.join(__dirname, "public");
+const publicDirectory = path.join(__dirname, "public"),
+  partialPath = path.join(__dirname, "/views/inc");
+
+hbs.registerPartials(partialPath);
 
 app.use(express.static(publicDirectory));
 
@@ -58,43 +61,6 @@ app.get("/Stroud", async (req, res) => {
   }
 });
 
-app.get("/forecast", (req, res) => {
-  res.render("forecast");
-});
-
-app.post("/forecast", async (req, res) => {
-  const city = req.body.cityInput,
-    country = req.body.countryInput;
-  try {
-    const myApi = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`
-    );
-    const lat = myApi.data.coord.lat,
-      lon = myApi.data.coord.lon;
-    const myApi2 = await axios.get(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${apiKey}&units=metric`
-    );
-    const daily = myApi2.data.daily;
-    const days = daily.map((a) => {
-      let date = new Date(a.dt * 1000),
-        day = weekdays[date.getUTCDay()];
-      return {
-        day: day,
-        temp: a.temp.day,
-        description: a.weather[0].description,
-        icon: `http://openweathermap.org/img/wn/${a.weather[0].icon}@2x.png`,
-      };
-    });
-    res.render("forecastResults", {
-      city: city,
-      country: country,
-      weatherObject: days,
-    });
-  } catch (err) {
-    console.log(err.request);
-  }
-});
-
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -102,21 +68,52 @@ app.get("/", (req, res) => {
 app.post("/", async (req, res) => {
   const city = req.body.cityInput,
     country = req.body.countryInput;
-  try {
-    const myApi = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`
-    );
 
-    res.render("searchResult", {
-      city: city,
-      country: country,
-      temperature: myApi.data.main.temp,
-      description: myApi.data.weather[0].description,
-      img: `http://openweathermap.org/img/wn/${myApi.data.weather[0].icon}@2x.png`,
-    });
-  } catch (err) {
-    console.error(err);
-    res.render("problem");
+  if (req.body.searchType == "current") {
+    try {
+      const myApi = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`
+      );
+
+      res.render("searchResult", {
+        city: city,
+        country: country,
+        temperature: myApi.data.main.temp,
+        description: myApi.data.weather[0].description,
+        img: `http://openweathermap.org/img/wn/${myApi.data.weather[0].icon}@2x.png`,
+      });
+    } catch (err) {
+      res.render("problem");
+    }
+  } else {
+    try {
+      const myApi = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`
+      );
+      const lat = myApi.data.coord.lat,
+        lon = myApi.data.coord.lon;
+      const myApi2 = await axios.get(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${apiKey}&units=metric`
+      );
+      const daily = myApi2.data.daily;
+      const days = daily.map((a) => {
+        let date = new Date(a.dt * 1000),
+          day = weekdays[date.getUTCDay()];
+        return {
+          day: day,
+          temp: a.temp.day,
+          description: a.weather[0].description,
+          icon: `http://openweathermap.org/img/wn/${a.weather[0].icon}@2x.png`,
+        };
+      });
+      res.render("forecastResults", {
+        city: city,
+        country: country,
+        weatherObject: days,
+      });
+    } catch (err) {
+      res.render("problem");
+    }
   }
 });
 

1cb25fad82432af0a6e7ca659ff2fe379be5e1fc
Alex Phimister <wphimmy@gmail.com>
2020-12-11 09:32:09 +0000

    Working searchable forecast for city+country. Error handling is dodgy

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./.env" });
 
 const apiKey = process.env.WEATHER_KEY,
-  lat = 51.7457,
-  lon = 2.2178,
   weekdays = [
     "Sunday",
     "Monday",
@@ -19,6 +17,7 @@ const apiKey = process.env.WEATHER_KEY,
     "Saturday",
   ];
 
+console.log(apiKey);
 const viewsPath = path.join(__dirname, "/views");
 
 app.set("view engine", "hbs");
@@ -33,13 +32,76 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/Stroud", async (req, res) => {
+  try {
+    const lat = 51.7457,
+      lon = 2.2178;
+    const myApi = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${apiKey}&units=metric`
+    );
+    const daily = myApi.data.daily;
+
+    const days = daily.map((a) => {
+      let date = new Date(a.dt * 1000),
+        day = weekdays[date.getUTCDay()];
+      return {
+        day: day,
+        temp: a.temp.day,
+        description: a.weather[0].description,
+        icon: `http://openweathermap.org/img/wn/${a.weather[0].icon}@2x.png`,
+      };
+    });
+    res.render("stroudWeather", { weatherObject: days });
+  } catch (err) {
+    console.log(err);
+    res.send(`<h1>${err.response.data.message}</h1>`);
+  }
+});
+
+app.get("/forecast", (req, res) => {
+  res.render("forecast");
+});
+
+app.post("/forecast", async (req, res) => {
+  const city = req.body.cityInput,
+    country = req.body.countryInput;
+  try {
+    const myApi = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`
+    );
+    const lat = myApi.data.coord.lat,
+      lon = myApi.data.coord.lon;
+    const myApi2 = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${apiKey}&units=metric`
+    );
+    const daily = myApi2.data.daily;
+    const days = daily.map((a) => {
+      let date = new Date(a.dt * 1000),
+        day = weekdays[date.getUTCDay()];
+      return {
+        day: day,
+        temp: a.temp.day,
+        description: a.weather[0].description,
+        icon: `http://openweathermap.org/img/wn/${a.weather[0].icon}@2x.png`,
+      };
+    });
+    res.render("forecastResults", {
+      city: city,
+      country: country,
+      weatherObject: days,
+    });
+  } catch (err) {
+    console.log(err.request);
+  }
+});
+
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/search", async (req, res) => {
-  const city = req.body.cityInput;
-  const country = req.body.countryInput;
+app.post("/", async (req, res) => {
+  const city = req.body.cityInput,
+    country = req.body.countryInput;
   try {
     const myApi = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=metric`
@@ -55,31 +117,6 @@ app.post("/search", async (req, res) => {
   } catch (err) {
     console.error(err);
     res.render("problem");
-  }
-});
-
-app.get("/Stroud", async (req, res) => {
-  try {
-    const myApi = await axios.get(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${apiKey}0&units=metric`
-    );
-    const daily = myApi.data.daily;
-
-    const days = daily.map((a) => {
-      console.log(a);
-      let date = new Date(a.dt * 1000);
-      let day = weekdays[date.getUTCDay()];
-      return {
-        day: day,
-        temp: a.temp.day,
-        description: a.weather[0].description,
-        icon: `http://openweathermap.org/img/wn/${a.weather[0].icon}@2x.png`,
-      };
-    });
-    res.render("stroudWeather", { weatherObject: days });
-  } catch (err) {
-    console.log(err);
-    res.send(`<h1>${err.response.data.message}</h1>`);
   }
 });
 
